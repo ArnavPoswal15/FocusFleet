505e9c806b8a234f5b8588c2c0c96ecdc329ff55
Arnav Poswal <128905909+ArnavPoswal15@users.noreply.github.com>
2026-09-04 18:00:44 +0530

    code update

--- web/focusfleet/detection.py
+++ web/focusfleet/detection.py
@@ -34,7 +34,7 @@ _face_cascade = cv2.CascadeClassifier(_haar_cascade_path)
 _face_mesh = mp.solutions.face_mesh.FaceMesh(
     static_image_mode=False,
     max_num_faces=1,
-    refine_landmarks=False,
+    refine_landmarks=True,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5,
 )
